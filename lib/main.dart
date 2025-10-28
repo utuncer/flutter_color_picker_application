16c2dae0614412faf2b1c05c8adcbb3ab931ec03
Umut Tunçer <utuncerx@gmail.com>
2025-10-28 16:05:06 +0300

    Add color picker functionality with toast notifications and random color selection

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:math';
+
+import 'package:fluttertoast/fluttertoast.dart';
+
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
@@ -25,6 +29,28 @@ class ColorPickerPage extends StatefulWidget {
 }
 
 class _ColorPickerPageState extends State<ColorPickerPage> {
+  void _renginKodunuGoster() {
+    Fluttertoast.showToast(
+      msg: "This is Center Short Toast",
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.CENTER,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.red,
+      textColor: Colors.white,
+      fontSize: 16.0,
+    );
+  }
+
+  void rastgeleSayiSec() {
+    final colors = renkler.keys.toList();
+    final rastgeleSayi = Random().nextInt(colors.length);
+    final randomColor = colors[rastgeleSayi];
+    setState(() {
+      selectedColor = randomColor;
+      //  debugPrint(rastgeleSayi.toString());
+    });
+  }
+
   Color selectedColor = Colors.red;
 
   final Map<Color, String> renkler = {
@@ -60,30 +86,45 @@ class _ColorPickerPageState extends State<ColorPickerPage> {
             ),
             SizedBox(height: 10),
             Text(renkler[selectedColor]!),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                DropdownButton<Color>(
-                  value: selectedColor,
-                  items: renkler.entries.map((entry) {
-                    return DropdownMenuItem(
-                      value: entry.key,
-                      child: Row(
-                        children: [
-                          Container(width: 20, height: 20, color: entry.key),
-                          SizedBox(width: 4),
-                          Text(entry.value),
-                        ],
-                      ),
-                    );
-                  }).toList(),
-                  onChanged: (value) {
-                    setState(() {
-                      selectedColor = value!;
-                    });
-                  },
-                ),
-              ],
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  DropdownButton<Color>(
+                    value: selectedColor,
+                    items: renkler.entries.map((entry) {
+                      return DropdownMenuItem(
+                        value: entry.key,
+                        child: Row(
+                          children: [
+                            Container(width: 20, height: 20, color: entry.key),
+                            SizedBox(width: 4),
+                            Text(entry.value),
+                          ],
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        selectedColor = value!;
+                      });
+                    },
+                  ),
+                  ElevatedButton(
+                    onPressed: rastgeleSayiSec,
+                    child: Text("Rastgele"),
+                  ),
+                  IconButton(
+                    onPressed: _renginKodunuGoster,
+                    icon: Icon(Icons.info),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.circle_outlined),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
